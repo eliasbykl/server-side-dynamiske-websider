d51e1b363ee3e109fa74ef0f6c9bb4a7c6583642
eliasbykl <elias.byklum@gmail.com>
2026-01-28 12:40:39 +0100

    Add /deltagere-1 route with list of students and configure .gitignore

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/her', (req, res) => {
     `);
 });
 
+app.get('/deltagere-1', (req, res) => {
+    res.send(`
+        <h1>Elever i klassen</h1>
+        <ul>
+            <li>Emma Hansen</li>
+            <li>Jonas Olsen</li>
+            <li>Maria Andersen</li>
+            <li>Kai Johansen</li>
+        </ul>
+    `);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
